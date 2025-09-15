4fecfafac560849098e9dbf9b411283edc6347ad
Allanfatso <126181649+Allanfatso@users.noreply.github.com>
2025-09-15 12:44:54 +0100

    Delete laravel-reel-planet-app directory

--- laravel-reel-planet-app/resources/views/reviews/partials/new-review-trigger.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="relative flex items-center pt-2 pb-8 px-3 rounded-lg transition bg-white border border-gray-300 dark:border-gray-700 dark:bg-gray-900 hover:bg-opacity-70">
-    <a class="text-gray-500" href="{{ route('review.create') }}">
-        {{ __('Create a Review') }}
-        <span class="absolute inset-0"></span>
-    </a>
-</div>
