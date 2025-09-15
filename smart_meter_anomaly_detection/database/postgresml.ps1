25ca7439a9dc4c8eb0ef51569bd16b826f3e5187
Allanfatso <a.f.makwembere@wlv.ac.uk>
2025-09-15 12:55:04 +0100

    adding app folder to laravel app

--- smart_meter_anomaly_detection/database/postgresml.ps1
+++
@@ -1,4 +0,0 @@
-docker run -d --name postgresml \
-  -p 5432:5432 \
-  -e POSTGRES_PASSWORD=password \
-  ghcr.io/postgresml/postgresml:latest
--- smart_meter_anomaly_detection/database/speed_up.sql
+++
@@ -1,6 +0,0 @@
-ANALYZE Household_details;
-ANALYZE Dates_and_Holidays;
-ANALYZE Tariffs;
-ANALYZE Weather;
-ANALYZE Consumption_data;
-VACUUM ANALYZE Consumption_data;
